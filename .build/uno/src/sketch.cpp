663864be8533d10ecc9772dd1e47a7ad9f8f8b46
Kent Ma <tnekent@gmail.com>
2014-01-09 12:16:18 -0500

    Added build folder

---
+++ .build/uno/src/sketch.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "rover.h"
+
+#line 1 "src/sketch.ino"
+/*
+ * Falconia Rover Project
+ * ~~~~~~~~~~~~~~~~~~~~~~~
+ * A rover for collecting data of humidity of the area, as well as testing the
+ * moisture of soil in the area.
+ *
+ *  Kent Ma, Alex Ives, Angela Zhou
+ *  Block Y
+ */
+#pragma once
+#ifndef SKETCH_H
+#define SKETCH_H
+//#include "rover.h"
+#endif
