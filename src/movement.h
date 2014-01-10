fa653be684d59741d6de93bd3ce6b5ac49130717
Kent Ma <tnekent@gmail.com>
2014-01-10 06:42:29 -0500

    Edited comments to fit style standards

--- src/movement.h
+++ src/movement.h
@@ -2,25 +2,25 @@
     #define _MOVEMENT_H
     /*
      * Motors for the driving of the rover. These pins are used by the Arduino
-     * motor shield. Pin A on the shield uses Digital 3, 9, and 12, while Pin B on 
-     * the shield uses Digital 8, 11, 13. We use Pin A for the left motor (LM), and 
-     * Pin B for the right motor (RM)
+     * motor shield. Pin A on the shield uses Digital 3, 9, and 12, while Pin B 
+     * on the shield uses Digital 8, 11, 13. We use Pin A for the left motor 
+     * (LM), and Pin B for the right motor (RM)
      *
      * PWM Pins - Digital 3, 11
-     *     These pins control the speed of the motor by writing an analog value from
-     *     0 to 255, 0 being off and 255 being at maximum power.
+     *     These pins control the speed of the motor by writing an analog value 
+     *     from 0 to 255, 0 being off and 255 being at maximum power.
      * 
      * DIR Pins - Digital 12, 13
-     *     These pins control the direction of the motor -- sending a HIGH signal
-     *     moves the motor in one direction, while a LOW signal reverses the motor 
-     *     to spin in another direction. For convenience, we define moving forward
-     *     (FRWD) as HIGH, while backwards (BACK) as LOW.
+     *     These pins control the direction of the motor -- sending a HIGH 
+     *     signal moves the motor in one direction, while a LOW signal reverses 
+     *     the motor to spin in another direction. For convenience, we define 
+     *     moving forward (FRWD) as HIGH, while backwards (BACK) as LOW.
      *
      * BRAKE Pins - Digital 8, 9
-     *     These pins can be used to brake the motor - sending a HIGH signal to them
-     *     brakes its corresponding motor. This differs from sending a 0 signal to
-     *     the PWM pin, as it also stops the motor, rather than letting it slow down
-     *     by cutting power.
+     *     These pins can be used to brake the motor - sending a HIGH signal to 
+     *     them brakes its corresponding motor. This differs from sending a 0 
+     *     signal to the PWM pin, as it also stops the motor, rather than 
+     *     letting it slow down by cutting power.
      */
     #define FRWD HIGH
     #define BACK LOW
